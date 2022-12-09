f97c371988920d282d1044dbd5474c91114f4000
joy <letimjoy7@gmail.com>
2022-12-09 13:15:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
+function reverseString(word) {
 
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
 /* 
   Add your pseudocode here
 */
